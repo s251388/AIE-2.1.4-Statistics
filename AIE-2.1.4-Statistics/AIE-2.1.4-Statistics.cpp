656a0d6fb2af75eaa7e8e403cda33eda33c7107d
s251388 <s251388@students.aie.edu.au>
2026-03-03 00:41:53 +1100

    Small change
    Hadn't written in the actual percentage calculation yet, or written a comment for it.

--- AIE-2.1.4-Statistics/AIE-2.1.4-Statistics.cpp
+++ AIE-2.1.4-Statistics/AIE-2.1.4-Statistics.cpp
@@ -7,9 +7,13 @@ using namespace std;
 
 int Accuracy(int Shots, int Hits) {
 
+	assert(Shots >= Hits >= 0);
+
 	int Percentage = 0;
 
-	assert(Shots >= Hits >= 0);
+	if (Hits > 0) {
+		Percentage = 100 * Hits / Shots;		// To avoid the entire percentage from being cut off, i multiply the Hits by 100 first, this only cuts
+	}											// off ("rounds down", in a way) any decimal value that would have been attached to the percentage.
 
 	return Percentage;
 }

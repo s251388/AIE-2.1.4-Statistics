f9f9ebd41695ae56e7cdc526c930dc4e56447195
s251388 <s251388@students.aie.edu.au>
2026-03-03 00:32:53 +1100

    Initial commit

---
+++ AIE-2.1.4-Statistics/AIE-2.1.4-Statistics.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+
+int Accuracy(int Shots, int Hits) {
+
+	int Percentage = 0;
+
+	assert(Shots >= Hits >= 0);
+
+	return Percentage;
+}
+
+int main()
+{
+    
+
+
+}
+
